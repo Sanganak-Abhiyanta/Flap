2f78d5b830cdd6bc5d9d993fab9b06f8111b2b6a
Sanganak Abhiyanta <imsanganakabhiyanta@gmail.com>
2022-01-03 23:23:09 +0530

    Third Commit. In This Commit I Have Improve Application Functionality and there is some error that data in help me box not submitted in firebase.Unable to solve problem

--- app/src/main/java/com/example/flap/ui/home/HelpBox/HelpMeActivity.java
+++ app/src/main/java/com/example/flap/ui/home/HelpBox/HelpMeActivity.java
@@ -1,12 +1,17 @@
 package com.example.flap.ui.home.HelpBox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.flap.MainActivity;
 import com.example.flap.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DatabaseReference;
@@ -15,7 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.Objects;
 
 public class HelpMeActivity extends AppCompatActivity {
-    private TextInputEditText name, emailAddress, phoneNumber, message;
+    private TextInputEditText nameEt, emailAddressEt, phoneNumberEt, messageEt;
     private MaterialButton submitQuery;
     //    private FirebaseDatabase database;
     private DatabaseReference reference, databaseReference;
@@ -27,57 +32,66 @@ public class HelpMeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_help_me);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Help Me");
 
-        name = findViewById(R.id.helpMeName);
-        emailAddress = findViewById(R.id.helpMeEmail);
-        phoneNumber = findViewById(R.id.helpMePhoneNumber);
-        message = findViewById(R.id.helpMeMessage);
+        nameEt = findViewById(R.id.helpMeName);
+        emailAddressEt = findViewById(R.id.helpMeEmail);
+        phoneNumberEt = findViewById(R.id.helpMePhoneNumber);
+        messageEt = findViewById(R.id.helpMeMessage);
 
         submitQuery = findViewById(R.id.submitHelpMe);
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("Help Me");
 
-        submitQuery.setOnClickListener(v -> validateForm());
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("HelpMe");
+
         progressDialog = new ProgressDialog(getApplicationContext());
         progressDialog.setTitle("Sending Message to Our Team!");
         progressDialog.setMessage("Please Wait While Sending Data :)");
+
+        submitQuery.setOnClickListener(v -> {
+            if (nameEt.getText().toString().isEmpty()) {
+//                Toast.makeText(getApplicationContext(), "Name Can't be Empty!", Toast.LENGTH_SHORT).show();
+                nameEt.setError("Empty");
+                nameEt.requestFocus();
+            } else if (emailAddressEt.getText().toString().isEmpty()) {
+//                Toast.makeText(getApplicationContext(), "Email Can't be Empty!", Toast.LENGTH_SHORT).show();
+                emailAddressEt.setError("Empty");
+                emailAddressEt.requestFocus();
+            } else if (phoneNumberEt.getText().toString().isEmpty()) {
+//                Toast.makeText(getApplicationContext(), "Phone Number Can't be Empty!", Toast.LENGTH_SHORT).show();
+                phoneNumberEt.setError("Empty");
+                phoneNumberEt.requestFocus();
+            } else if (messageEt.getText().toString().isEmpty()) {
+//                Toast.makeText(getApplicationContext(), "Message Can't be Empty!", Toast.LENGTH_SHORT).show();
+                messageEt.setError("Empty");
+                messageEt.requestFocus();
+            } else {
+                progressDialog.show();
+                insertData();
+            }
+        });
+
     }
 
-    private void validateForm() {
-        if (Objects.requireNonNull(name.getText()).toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Name Can't be Empty!", Toast.LENGTH_SHORT).show();
-            name.setError("Empty");
-            name.requestFocus();
-        } else if (Objects.requireNonNull(emailAddress.getText()).toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Email Can't be Empty!", Toast.LENGTH_SHORT).show();
-            emailAddress.setError("Empty");
-            emailAddress.requestFocus();
-        } else if (Objects.requireNonNull(phoneNumber.getText()).toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Phone Number Can't be Empty!", Toast.LENGTH_SHORT).show();
-            phoneNumber.setError("Empty");
-            phoneNumber.requestFocus();
-        } else if (Objects.requireNonNull(message.getText()).toString().isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Message Can't be Empty!", Toast.LENGTH_SHORT).show();
-            message.setError("Empty");
-            message.requestFocus();
-        } else {
-            progressDialog.show();
-            final String uniqueKey = databaseReference.push().getKey();
-            assert uniqueKey != null;
-            reference = databaseReference.child(uniqueKey);
-            HelpModel helpModel = new HelpModel(name.getText().toString(), emailAddress.getText().toString(),
-                    phoneNumber.getText().toString(), message.getText().toString(), uniqueKey);
+    private void insertData() {
 
-            reference.setValue(helpModel).addOnCompleteListener(task -> {
+        final String uniqueKey = reference.push().getKey();
+        reference = databaseReference.child(uniqueKey);
+//        DoctorsData doctorsData = new DoctorsData(name, experience, address, timing, rating, downloadUrl, uniqueKey, fee);
+        HelpModel helpModel=new HelpModel(nameEt.getText().toString(),emailAddressEt.getText().toString(),phoneNumberEt.getText().toString(),messageEt.getText().toString(),uniqueKey);
+        reference.child(uniqueKey).setValue(helpModel).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
                 progressDialog.dismiss();
-                name.setText("");
-                emailAddress.setText("");
-                phoneNumber.setText("");
-                message.setText("");
-//                    startActivity(new Intent(getApplicationContext(), MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
-            }).addOnFailureListener(e -> {
+                emailAddressEt.setText("");
+                nameEt.setText("");
+                phoneNumberEt.setText("");
+                messageEt.setText("");
+                Toast.makeText(getApplicationContext(), "Query Submitted!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
                 progressDialog.dismiss();
                 Toast.makeText(getApplicationContext(), "Something went wrong!", Toast.LENGTH_SHORT).show();
-            });
-        }
-
+            }
+        });
     }
 }
